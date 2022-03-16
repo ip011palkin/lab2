87cd795e4cd76b3342469c05ca0b1c72727059ea
Палкин ИП-011 <aa.palkin10@gmail.com>
2022-03-16 16:38:33 +0300

    добавил тесты и операторы

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,10 +15,10 @@ namespace ConsoleApp2
                 // RomanNumber nr3 = new RomanNumber(92);
                 RomanNumber number4 = new RomanNumber(3);
                 RomanNumber number5 = new RomanNumber(8);
-                Console.WriteLine($"addition: {number1.ToString()} + {number2.ToString()} = {RomanNumber.Add(number1, number2).ToString()}");
-                Console.WriteLine($"subtraction: {number1.ToString()} - {number3.ToString()} = {RomanNumber.Sub(number1, number3).ToString()}");
-                Console.WriteLine($"multiplication: {number1.ToString()} * {number4.ToString()} = {RomanNumber.Mul(number1, number4).ToString()}");
-                Console.WriteLine($"division: {number1.ToString()} / {number5.ToString()} = {RomanNumber.Div(number1, number5).ToString()}");
+                Console.WriteLine($"addition: {number1.ToString()} + {number2.ToString()} = {(number1 + number2).ToString()}");
+                Console.WriteLine($"subtraction: {number1.ToString()} - {number3.ToString()} = {(number1 - number3).ToString()}");
+                Console.WriteLine($"multiplication: {number1.ToString()} * {number4.ToString()} = {(number1 * number4).ToString()}");
+                Console.WriteLine($"division: {number1.ToString()} / {number5.ToString()} = {(number1 / number5).ToString()}");
                 RomanNumber[] array = { number1, number2, number3, number4, number5 };
                 Array.Sort(array);
                 Console.WriteLine("sort result");
---
+++ ConsoleApp2Tests/RomanNumberTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void IsNaturalNumberTest()
+        {
+            Assert.ThrowsException<RomanNumberException>(() => (new RomanNumber(0)), "entered natural number");
+        }
+
+        [TestMethod()]
+        public void AdditionTest()
+        {
+            var n1 = new RomanNumber(15);
+            var n2 = new RomanNumber(21);
+            var expected = new RomanNumber(36);
+            RomanNumber actual = n1 + n2;
+            Assert.AreEqual(expected.ToString(), actual.ToString(), "addition failed");
+        }
+
+        [TestMethod()]
+        public void SubstractionTests()
+        {
+            var n1 = new RomanNumber(21);
+            var n2 = new RomanNumber(15);
+            var expected = new RomanNumber(6);
+            RomanNumber actual = n1 - n2;
+            Assert.AreEqual(expected.ToString(), actual.ToString(), "substraction failed");
+            Assert.ThrowsException<RomanNumberException>(() => (n2 - n1), "number is less or equal to 0");
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var n1 = new RomanNumber(20);
+            var n2 = new RomanNumber(5);
+            var expected = new RomanNumber(100);
+            RomanNumber actual = n1 * n2;
+            Assert.AreEqual(expected.ToString(), actual.ToString(), "multiplication failed");
+        }
+
+        [TestMethod()]
+        public void DivisionTests()
+        {
+            var n1 = new RomanNumber(100);
+            var n2 = new RomanNumber(5);
+            var expected = new RomanNumber(20);
+            RomanNumber actual = n1 / n2;
+            Assert.AreEqual(expected.ToString(), actual.ToString(), "division error");
+            Assert.ThrowsException<RomanNumberException>(() => (n2 / n1), "number is less or equal to 0");
+        }
+
+        [TestMethod()]
+        public void ToStringFunctionTest()
+        {
+            RomanNumber n = new RomanNumber(27);
+            string expected = "XXVII";
+            string actual = n.ToString();
+            Assert.AreEqual(expected, actual, "translation to string error");
+        }
+
+        [TestMethod()]
+        public void CloneFunctionTests()
+        {
+            RomanNumber n = new RomanNumber(27);
+            RomanNumber expected = new RomanNumber(27);
+            RomanNumber clone = (RomanNumber)n.Clone();
+            Assert.AreEqual(expected.ToString(), clone.ToString(), "clone error");
+        }
+
+        [TestMethod()]
+        public void CompareToFunctionTest()
+        {
+            RomanNumber n = new RomanNumber(10);
+            RomanNumber n1 = new RomanNumber(10);
+            const int expected = 0;
+            int actual = n.CompareTo(n1);
+            Assert.AreEqual(expected, actual, "compare error");
+        }
+    }
+}
